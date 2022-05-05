ebbebd4b47842aa949e088e53b2b4b82bcd698bd
Klaudia <buryklaudia2@gmail.com>
2022-05-05 18:08:10 +0200

    Added exercise 1

---
+++ RR_May_04_05/ex_1.py
@@ -0,0 +1,27 @@
+import unittest
+import sys
+
+def convert(f, target='c'):
+    if target == 'c':
+        return (f - 32) / 1.8
+    elif target == 'k':
+        return ((f - 32) / 1.8) + 273.15
+    else:
+        raise Exception('wrong target')
+
+class TestDivide(unittest.TestCase):
+    def test_example(self):
+        self.assertEqual(convert(50, target='c'), 10)
+        self.assertEqual(convert(70, target='c'), 21.1111111111111111)
+        self.assertEqual(convert(90, target='c'), 32.2222222222222222)
+        
+    def test_Reaumur_scale(self):
+        with self.assertRaises(Exception):
+            convert(50, target = 'Reaumur')
+     
+
+
+
+if __name__ == '__main__':
+    unittest.main()
+ 
